58a37b3b54fbcc1a376e0ee3e224ff4c3264f786
binarytrance <ganeshan.dash@gmail.com>
2021-09-30 02:57:27 +0530

    initial commit

---
+++ src/components/Todos.tsx
@@ -0,0 +1,12 @@
+const Todos: React.FC<{items: string[]}> = () => {
+    return (
+        <ul>
+           <li>Learn TS</li>
+           <li>Learn Karate</li>
+        </ul>
+    );
+};
+
+
+
+export default Todos;
